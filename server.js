e8cc1085d35e8d22809a4abefb0f8e31bb4dd708
akshatmalik <akshat.ndun@gmail.com>
2017-04-14 14:25:48 +0530

    Implemented Sever

--- server.js
+++ server.js
@@ -1,9 +1,58 @@
+//modules
 var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
 
-http.createServer(function (req,res) 
-{
-	res.writeHead(200,{'Cotent-Type':'text/plain'});
-	res.end("Hello World\n");
-}).listen(1337,'127.0.0.1');
+//array of mime types
+var mimeTypes = {
+	'html' : 'text/html',
+	'jpeg' : 'image/jpeg',
+	'jpg'  : 'image/jpg',
+	'js'   : 'text/javascrpit',
+	'css'  : 'text/css'
+};
 
-console.log("Started server\n");
+//creating the webserver
+
+http.createServer(function(req, res)
+	{
+		var uri = url.parse(req.url).pathname;
+		var filename = path.join(process.cwd(),unescape(uri));
+		console.log("Loading: " + uri);
+		var stats;
+
+		try
+		{
+			stats = fs.lstatSync(filename);
+		}
+		catch(e)
+		{
+			res.writeHead(404,{'Content-type' : 'text-plain'});
+			res.write("404 Not Found\n");
+			res.end();
+			return;
+		}
+
+		if(stats.isFile())
+		{
+			var mimeType = mimeTypes[path.extname(filename).split('.').reverse()[0]];
+			res.writeHead(200,{'Content-Type' : mimeType});
+
+			var fileStream = fs.createReadStream(filename);
+			fileStream.pipe(res);
+
+		}
+
+		else if(stats.isDirectory())
+		{
+			res.writeHead(302, {'Location' : 'index.html'});
+			res.end();
+		}
+		else
+		{
+			res.writeHead(500,{'Content-Type':'text-plain'});
+			res.write("500 Internal error\n");
+			res.end();
+		}
+	}).listen(3000);
